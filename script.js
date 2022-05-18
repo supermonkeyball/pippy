c946bd1d1a14b630d60838a02829452d350848f0
Glitch (glitch-hello-website) <none>
2022-05-18 23:57:00 +0000

    🛀🌚 Checkpoint ./script.js:22332890/1949 ./style.css:22332890/980 ./index.html:22332890/1091

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function hungry() {
-  let x = HUNGER-METER WIDTH
-  document.getElementById("hunger-meter").width = ${x - 0.5}vh;
+  var meter = document.getElementById("hunger-meter");
+  var current_length = meter.offsetWidth;
+
+  console.log(current_length);
+
+  if (current_length == 0) {
+    console.log("death");
+    clearInterval(the_depletion_of_hunger);
+  }
+
+  var new_length = current_length - 4;
+
+  if (new_length < 0) {
+    new_length = 0;
+  }
+
+  meter.style.width = new_length + "px";
 }
 
-setInterval(hungry, 500);
+const the_depletion_of_hunger = setInterval(hungry, 2000);
+window.onload = the_depletion_of_hunger;
+
+
+
+function eat() {
+  var chicken = document.getElementById("chicken")
+  
+  console.log("Clicked");
+  
+  
+  
+}
+
+button.addEventListener("click", eat);
