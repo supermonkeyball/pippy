7083b1a4fab5bcbd46c9d169c405da8e8a4af9a0
Glitch (glitch-hello-website) <none>
2022-05-20 01:58:32 +0000

    🚌💡 Checkpoint ./style.css:22332890/134 ./script.js:22332890/2411

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function hungry() {
   if (current_length == 0) {
     console.log("death");
     clearInterval(the_depletion_of_hunger);
-    chicken.removeEventListener("click", eat)
+    chicken.removeEventListener("click", eat);
   }
 
   var new_length = current_length - 4;
@@ -31,6 +31,10 @@ function eat() {
   var current_length = meter.offsetWidth;
   var new_length = current_length + 12;
 
+  if (new_length > 100) {
+    new_length = 100;
+  }
+
   meter.style.width = new_length + "px";
 }
 
@@ -38,6 +42,12 @@ chicken.addEventListener("click", eat);
 
 function mood() {
   var pippy = document.getElementById("pippy");
-  var happy = "https://cdn.glitch.global/f991fb0b-d232-4af4-8263-8db275e14328/trim.0E08199C-AAEA-4D2A-B8EE-5B093ED9848D.MOV?v=1653010433703";
-  var sad = ""
+  var happy =
+    "https://cdn.glitch.global/f991fb0b-d232-4af4-8263-8db275e14328/trim.0E08199C-AAEA-4D2A-B8EE-5B093ED9848D.MOV?v=1653010433703";
+  var sad = "";
 }
+
+// IF FEED = BLADDER DOWN
+// IF PLAY = HUNGER DOWN
+// IF 
+
