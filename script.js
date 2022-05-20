bb6100a0257219cdcfcbeb54c1b89bad8f46713a
Glitch (glitch-hello-website) <none>
2022-05-20 06:05:55 +0000

    🍔🖇 Checkpoint ./script.js:22332890/14804

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var pippy = document.getElementById("pippy");
+
 //stomach
 
 function stomach() {
@@ -91,6 +93,39 @@ function feelings() {
 const the_depletion_of_fun = setInterval(feelings, 4400);
 window.onload = the_depletion_of_fun;
 
+// stamina
+
+function stamina() {
+  var meter = document.getElementById("energy-meter");
+  var current_length = meter.offsetWidth;
+
+  console.log(current_length);
+
+  if (current_length == 0) {
+    console.log("death");
+    clearInterval(the_depletion_of_fun);
+    nyquil.removeEventListener("click", sleep);
+  }
+
+  var new_length = current_length - 2;
+
+  if (new_length < 0) {
+    new_length = 0;
+  }
+
+  if (current_length < 30) {
+    document.getElementById("pippy").src =
+      "https://cdn.glitch.global/f991fb0b-d232-4af4-8263-8db275e14328/55FCD628-4B8B-4D87-AEB9-5A8E46BCDE1E.gif?v=1653016028730";
+  }
+
+  meter.style.width = new_length + "px";
+}
+
+const the_depletion_of_energy = setInterval(stamina, 4400);
+window.onload = the_depletion_of_energy;
+
+
+
 // eat chicken, regain hunger (lose bladder)
 
 let chicken = document.getElementById("chicken");
@@ -98,7 +133,6 @@ let chicken = document.getElementById("chicken");
 function eat() {
   console.log("clicked");
 
-  var pippy = document.getElementById("pippy");
   var stomach = document.getElementById("hunger-meter");
   var current_length = stomach.offsetWidth;
   var new_length = current_length + 12;
@@ -131,7 +165,6 @@ let hydrant = document.getElementById("hydrant");
 function gopee() {
   console.log("clicked");
 
-  var pippy = document.getElementById("pippy");
   var bladder = document.getElementById("toilet-meter");
   var current_length = bladder.offsetWidth;
   var new_length = current_length + 20;
@@ -157,17 +190,16 @@ function gopee() {
 
 hydrant.addEventListener("click", gopee);
 
-// play, regain feelings (lose sta,mina)
+// play, regain feelings (lose stamina)
 
 let ball = document.getElementById("ball");
 
 function play() {
   console.log("clicked");
 
-  var pippy = document.getElementById("pippy");
-  var bladder = document.getElementById("fun-meter");
+  var feelings = document.getElementById("fun-meter");
   var current_length = feelings.offsetWidth;
-  var new_length = current_length + 20;
+  var new_length = current_length + 12;
 
   var stamina = document.getElementById("energy-meter");
   var stamina_before = stamina.offsetWidth;
@@ -184,15 +216,48 @@ function play() {
   document.getElementById("pippy").src =
     "https://cdn.glitch.global/f991fb0b-d232-4af4-8263-8db275e14328/CB92F614-5138-421B-B7ED-D9DA1771235D.gif?v=1653016002999";
 
-  bladder.style.width = new_length + "px";
-  feelings.style.width = feelings_after + "px";
+  feelings.style.width = new_length + "px";
+  stamina.style.width = stamina_after + "px";
 }
 
-hydrant.addEventListener("click", gopee);
+ball.addEventListener("click", play);
 
+// sleep, regain stamina (lose hunger)
 
+let nyquil = document.getElementById("nyquil");
 
+function sleep() {
+  console.log("clicked");
 
-// IF PLAY = HUNGER DOWN
-// IF SLEEP = PLAY DOWN
-// WHEN HUNGER/PLAY LOW = pippySad
+  var stamina = document.getElementById("energy-meter");
+  var current_length = stamina.offsetWidth;
+  var new_length = current_length + 12;
+
+  var hunger = document.getElementById("hunger-meter");
+  var hunger_before = hunger.offsetWidth;
+  var hunger_after = hunger_before - 12;
+
+  if (new_length > 100) {
+    new_length = 100;
+  }
+
+  setTimeout(function () {
+    document.getElementById("pippy").src =
+      "https://cdn.glitch.global/f991fb0b-d232-4af4-8263-8db275e14328/162C7935-0BA4-40CC-A51D-BF83BE2A0582.gif?v=1653015911736";
+  }, 1200);
+  document.getElementById("pippy").src =
+    "https://cdn.glitch.global/f991fb0b-d232-4af4-8263-8db275e14328/CB92F614-5138-421B-B7ED-D9DA1771235D.gif?v=1653016002999";
+
+  stamina.style.width = new_length + "px";
+  hunger.style.width = hunger_after + "px";
+}
+
+nyquil.addEventListener("click", sleep);
+
+function death() {
+  var stomach =
+  var bladder =
+  var feelings =
+  var 
+}
+
